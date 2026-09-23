2fa28a89f1ac7c990195656c7f09574671a0292c
MrPup-minecraftkus <nazarvalcuk1@gmail.com>
2026-09-23 21:28:34 +0300

    neoforge-26.1.2

--- src/main/java/net/mrpup/everyminingdimension/item/ModItems.java
+++ src/main/java/net/mrpup/everyminingdimension/item/ModItems.java
@@ -15,15 +15,15 @@ public class ModItems {
             DeferredRegister.createItems(EveryMiningDimension.MOD_ID);
 
     public static final DeferredHolder<Item, BlockItem> TELEPORT_OVERWORLD_MINING_DIMENSION = ITEMS.register(
-            "teleport_overworld_mining_dimension", id -> new BlockItem(ModBlocks.TELEPORT_OVERWORLD_MINING_DIMENSION.get(), new Item.Properties().stacksTo(1).setId(ResourceKey.create(Registries.ITEM, id)))
+            "teleport_overworld_mining_dimension", id -> new BlockItem(ModBlocks.TELEPORT_OVERWORLD_MINING_DIMENSION.get(), new Item.Properties().setId(ResourceKey.create(Registries.ITEM, id)))
     );
 
     public static final DeferredHolder<Item, BlockItem> TELEPORT_NETHER_MINING_DIMENSION = ITEMS.register(
-            "teleport_nether_mining_dimension", id -> new BlockItem(ModBlocks.TELEPORT_NETHER_MINING_DIMENSION.get(), new Item.Properties().stacksTo(1).setId(ResourceKey.create(Registries.ITEM, id)))
+            "teleport_nether_mining_dimension", id -> new BlockItem(ModBlocks.TELEPORT_NETHER_MINING_DIMENSION.get(), new Item.Properties().setId(ResourceKey.create(Registries.ITEM, id)))
     );
 
     public static final DeferredHolder<Item, BlockItem> TELEPORT_END_MINING_DIMENSION = ITEMS.register(
-            "teleport_end_mining_dimension", id -> new BlockItem(ModBlocks.TELEPORT_END_MINING_DIMENSION.get(), new Item.Properties().stacksTo(1).setId(ResourceKey.create(Registries.ITEM, id)))
+            "teleport_end_mining_dimension", id -> new BlockItem(ModBlocks.TELEPORT_END_MINING_DIMENSION.get(), new Item.Properties().setId(ResourceKey.create(Registries.ITEM, id)))
     );
 
     public static void register(IEventBus eventBus) {
